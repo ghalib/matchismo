3a0c1068c1088e5aac6f5130fadd08605b53554c
Ghalib Suleiman <ghalib@sent.com>
2013-11-06 16:03:16 -0800

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by ghalib on 11/6/13.
+//  Copyright (c) 2013 Zift LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
